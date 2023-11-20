c8cd79f3d54b30581d828ee13ddd88939a8a8807
Steven Espinoza <espisteven5@gmail.com>
2023-11-19 17:54:12 -0800

    passing test

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,10 @@
+// Article.test.js in src/__tests__
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
-
 import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            hello from the Header!
+        </header>
+    );
+}
+
+export default Header;
